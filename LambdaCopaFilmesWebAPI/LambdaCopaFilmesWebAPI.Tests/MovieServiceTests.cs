70302e31b1387da8008d413e61120f69530c09ab
Caio Silva <caiovosilva@outlook.com>
2019-11-18 17:06:04 -0300

    intro to unit testing

---
+++ LambdaCopaFilmesWebAPI/LambdaCopaFilmesWebAPI.Tests/MovieServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using LambdaCopaFilmesWebAPI.Services;
+using LambdaCopaFilmesWebAPI.Domain.Services;
+
+namespace LambdaCopaFilmesWebAPI.Tests
+{
+    public class MovieServiceTests
+    {
+        private readonly IMovieService MovieService;
+
+        public MovieServiceTests()
+        {
+            this.MovieService = new MovieService();
+        }
+
+
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = this.MovieService.RunChampionship();
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
